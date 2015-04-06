d35021469d676a92335a5e72036db7e90a3205b9
Kevin Jude A. Deloria <akiwarheit@gmail.com>
2015-04-06 17:04:18 +0800

    Formatting

--- MaterialDesign/src/com/gc/materialdesign/views/CheckBox.java
+++ MaterialDesign/src/com/gc/materialdesign/views/CheckBox.java
@@ -106,19 +106,19 @@ public class CheckBox extends CustomView {
         }
 	}
 
-    /**
-     * Apply textAppearance based from attribute set xml to a specific TextView
-     *
-     * @param textView
-     * @param attributeSet
-     */
-    private void applyTextAppearance(TextView textView, AttributeSet attributeSet) {
-        int textAppearanceResource = attributeSet.getAttributeResourceValue(ANDROIDXML, "textAppearance", -1);
+        /**
+         * Apply textAppearance based from attribute set xml to a specific TextView
+         *
+         * @param textView
+         * @param attributeSet
+         */
+        private void applyTextAppearance(TextView textView, AttributeSet attributeSet) {
+            int textAppearanceResource = attributeSet.getAttributeResourceValue(ANDROIDXML, "textAppearance", -1);
 
-        if (textAppearanceResource != -1) {
-            textView.setTextAppearance(getContext(), textAppearanceResource);
+            if (textAppearanceResource != -1) {
+                textView.setTextAppearance(getContext(), textAppearanceResource);
+            }
         }
-    }
 
 	@Override
 	public void invalidate() {
